74b5371e242797223f8305d6bb57b565a5211fc9
Camilo Sánchez Pérez <129013559+Kmisanchez0604@users.noreply.github.com>
2025-11-18 11:21:47 -0500

    Creación Login (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -10,16 +10,14 @@ import {
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { ellipse, square, triangle } from 'ionicons/icons';
+import Login from './pages/Login';
 import React, { useEffect } from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import Habits from './pages/Habits';
 import Home from './pages/Home';
 import Progress from './pages/Progress';
 
-/* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
-
-/* Basic CSS for apps built with Ionic */
 import '@ionic/react/css/normalize.css';
 import '@ionic/react/css/structure.css';
 import '@ionic/react/css/typography.css';
