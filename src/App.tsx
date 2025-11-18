8e94fc4a0efada0b408d48fddeb915359bf8d3f2
Camilo Sánchez Pérez <jeinnk1@hotmail.com>
2025-11-18 11:08:47 -0500

    Creación Login

--- src/App.tsx
+++ src/App.tsx
@@ -11,38 +11,23 @@ import {
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { ellipse, square, triangle } from 'ionicons/icons';
+
+import Login from './pages/Login';
 import Home from './pages/Home';
 import Habits from './pages/Habits';
 import Progress from './pages/Progress';
 
-/* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
-
-/* Basic CSS for apps built with Ionic */
 import '@ionic/react/css/normalize.css';
 import '@ionic/react/css/structure.css';
 import '@ionic/react/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
 import '@ionic/react/css/padding.css';
 import '@ionic/react/css/float-elements.css';
 import '@ionic/react/css/text-alignment.css';
 import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
-
-/**
- * Ionic Dark Mode
- * -----------------------------------------------------
- * For more info, please see:
- * https://ionicframework.com/docs/theming/dark-mode
- */
-
-/* import '@ionic/react/css/palettes/dark.always.css'; */
-/* import '@ionic/react/css/palettes/dark.class.css'; */
 import '@ionic/react/css/palettes/dark.system.css';
-
-/* Theme variables */
 import './theme/variables.css';
 
 setupIonicReact();
@@ -50,36 +35,47 @@ setupIonicReact();
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route exact path="/Home">
-            <Home />
-          </Route>
-          <Route exact path="/Habits">
-            <Habits />
-          </Route>
-          <Route path="/Progress">
-            <Progress />
-          </Route>
-          <Route exact path="/">
-            <Redirect to="/Home" />
-          </Route>
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="Home" href="/Home">
-            <IonIcon aria-hidden="true" icon={triangle} />
-            <IonLabel>Home</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="Habits" href="/Habits">
-            <IonIcon aria-hidden="true" icon={ellipse} />
-            <IonLabel>Habits</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="Progress" href="/Progress">
-            <IonIcon aria-hidden="true" icon={square} />
-            <IonLabel>Progress</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
+      {/* 🔹 Rutas principales */}
+      <IonRouterOutlet id="main">
+        {/* Rutas sin tabs */}
+        <Route exact path="/login" component={Login} />
+
+        {/* Rutas con tabs */}
+        <Route path="/tabs">
+          <IonTabs>
+            <IonRouterOutlet>
+              <Route exact path="/tabs/home" component={Home} />
+              <Route exact path="/tabs/habits" component={Habits} />
+              <Route exact path="/tabs/progress" component={Progress} />
+              <Route exact path="/tabs">
+                <Redirect to="/tabs/home" />
+              </Route>
+            </IonRouterOutlet>
+
+            <IonTabBar slot="bottom">
+              <IonTabButton tab="home" href="/tabs/home">
+                <IonIcon icon={triangle} />
+                <IonLabel>Home</IonLabel>
+              </IonTabButton>
+
+              <IonTabButton tab="habits" href="/tabs/habits">
+                <IonIcon icon={ellipse} />
+                <IonLabel>Habits</IonLabel>
+              </IonTabButton>
+
+              <IonTabButton tab="progress" href="/tabs/progress">
+                <IonIcon icon={square} />
+                <IonLabel>Progress</IonLabel>
+              </IonTabButton>
+            </IonTabBar>
+          </IonTabs>
+        </Route>
+
+        {/* Redirección inicial */}
+        <Route exact path="/">
+          <Redirect to="/login" />
+        </Route>
+      </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
 );
